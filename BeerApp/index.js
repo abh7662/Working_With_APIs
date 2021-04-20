8744c5ffa947cc280d415003e02a7edd93ae20f0
ak7662 <ak7662@gmail.com>
2021-04-20 20:50:05 +0530

    some more js added to beer app

--- BeerApp/index.js
+++ BeerApp/index.js
@@ -1,9 +1,6 @@
 // variables
 const urlBase = "https://api.punkapi.com/v2/beers";
 
-// Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
-// Render each beer name inside the div with the class of beers that currently exists in the HTML file.
-
 async function getBeers() {
     // fetch
     const beerPromise = await fetch(urlBase);
@@ -13,10 +10,29 @@ async function getBeers() {
     const beersDiv = document.querySelector('.beers');
     
     let beerHtml = "";
+      
+    // Fill in the blanks with the rest of the data
     
     beers.forEach(beer => {
        beerHtml += `
-        <h3>${beer.name}</h3>
+        <div class='beer-wrapper card'>
+            <div class='beer'>
+                <img class='beer__img' src="${beer.image_url}">
+                <h3>${beer.name}</h3>
+                <span class='beer__info'>
+                    <span>ABV: ${beer.abv}%</span>
+                    <span>IBU: ${beer.ibu}</span>
+                </span>
+            </div>
+            <div class='beer__content'>
+                <div class='beer__name'>${beer.name}</div>
+                <div class='beer__tagline'>${beer.tagline}</div>
+                <div class='beer__description'>${beer.description}</div>
+                <div class='beer__food-pairing'>
+                    Pair with: ${beer.food_pairing.join(', ')}
+                </div>
+            </div>
+        </div>
        `; 
     });
     
