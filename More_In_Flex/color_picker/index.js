f32b61dd3f67f1b33455787e4d784b6632c9ff1d
ak7662 <ak7662@gmail.com>
2021-04-23 17:01:00 +0530

    color code updated

--- More_In_Flex/color_picker/index.js
+++ More_In_Flex/color_picker/index.js
@@ -4,20 +4,34 @@
     Use FlexBox to create a Responsive 5x5 Grid displaying the colors    
 */
 
-function showColors(colors){
-    let htmlDiv = ""
-    colors.map(color => {
-         htmlDiv += `<div class="colors" style="background-color: ${color.value} "></div>`
-        //  htmlDiv += `<div style="background-color: ${colors[1].value} ">${colors[0].value}</div>`
+/* 
+    Create a variable to store the count of colors to be loaded
+    
+    Update the getColors() function to take that count as a parameter
+    
+    Set the count to 100 colors, update the .my-color class to maintain the grid structure
+*/
 
-         })
-    document.body.innerHTML = htmlDiv
-}
-async function getColors(){
-    const colors = await fetch("https://apis.scrimba.com/hexcolors?count=25")
-                        .then(response => response.json())
-                        .then(data => data)
-    showColors(colors.colors)
+const color_count = 100
+
+function displayColors(colors) {
+    let myColorsHtml = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}"></div>`
+    }).join('')
+    
+    console.log(myColorsHtml)
+
+    document.body.innerHTML = `<div class="my-colors">
+        ${myColorsHtml}
+    </div>`
 }
 
-getColors()
+async function getColors(color_count) {
+    let response = await fetch("https://apis.scrimba.com/hexcolors?count="+color_count)
+    let data = await response.json()
+    let colors = data.colors
+    
+    displayColors(colors)
+}
+
+getColors(color_count)
