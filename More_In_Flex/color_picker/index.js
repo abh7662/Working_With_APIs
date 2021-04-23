3211afbaa598248b22ef5237de03897f75020c37
ak7662 <ak7662@gmail.com>
2021-04-23 13:13:10 +0530

    color picker completed

--- More_In_Flex/color_picker/index.js
+++ More_In_Flex/color_picker/index.js
@@ -4,3 +4,20 @@
     Use FlexBox to create a Responsive 5x5 Grid displaying the colors    
 */
 
+function showColors(colors){
+    let htmlDiv = ""
+    colors.map(color => {
+         htmlDiv += `<div class="colors" style="background-color: ${color.value} "></div>`
+        //  htmlDiv += `<div style="background-color: ${colors[1].value} ">${colors[0].value}</div>`
+
+         })
+    document.body.innerHTML = htmlDiv
+}
+async function getColors(){
+    const colors = await fetch("https://apis.scrimba.com/hexcolors?count=25")
+                        .then(response => response.json())
+                        .then(data => data)
+    showColors(colors.colors)
+}
+
+getColors()
