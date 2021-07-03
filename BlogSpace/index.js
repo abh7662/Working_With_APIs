5b222ee8b7a732879643bc3cad310f03d910987b
ak7662 <ak7662@gmail.com>
2021-07-03 20:59:10 +0530

    styling in blogspace

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -13,3 +13,25 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts/")
 
       </div>`).join("")
     })
+
+function getIds(value){
+  return document.getElementById(value)
+}
+document.querySelector(".btn").addEventListener("click",()=>{
+  const title = getIds("title").value
+  const body = getIds("body").value
+  let myData = {
+    title,
+    body
+  }
+  console.log(myData)
+})
+    /**
+ Challenge:
+ 
+ * Listen for the "submit" event on the form (which will happen when the button is clicked)
+    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
+ * Combine the title value and body value into an object (with a "title" property and "body" property)
+ * Log the object to the console
+ * 
+ */
