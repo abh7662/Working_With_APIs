764e596cbfc1c8f511ee4ecbf2e9330d103415fe
ak7662 <ak7662@gmail.com>
2021-07-04 11:07:04 +0530

    blogspace completed

--- BlogSpace/index.js
+++ BlogSpace/index.js
@@ -1,32 +1,61 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts/")
-    .then(response => response.json())
-    .then(data => {
-      const postArr = data.slice(0,5)
-      document.querySelector("#main").innerHTML = postArr.map(arr => `
-      <div>
-        <h1>${arr.title}</h1>
+let postsArray = []
+
+function renderPosts(postsArray) {
+  document.querySelector("#main").innerHTML = postsArray
+    .map(
+      (arr) => `
+      <h1>${arr.title}</h1>
         <p>${arr.body}</p>
         <hr />
-        <h1>${arr.title}</h1>
-        <p>${arr.body}</p>
-        <hr />
-
-      </div>`).join("")
-    })
-
-function getIds(value){
-  return document.getElementById(value)
+      `
+    )
+    .join("");
 }
-document.querySelector(".btn").addEventListener("click",()=>{
-  const title = getIds("title").value
-  const body = getIds("body").value
-  let myData = {
+function clearForm() {
+  getIds("title").value = "";
+  getIds("body").value = "";
+}
+fetch("https://apis.scrimba.com/jsonplaceholder/posts/")
+  .then((response) => response.json())
+  .then((data) => {
+    postsArray = data.slice(0, 5);
+    renderPosts(postsArray);
+  })
+
+function getIds(value) {
+  return document.getElementById(value);
+}
+let myData = {};
+document.querySelector(".btn").addEventListener("click", () => {
+  const title = getIds("title").value;
+  const body = getIds("body").value;
+  myData = {
     title,
-    body
+    body,
   }
-  console.log(myData)
-})
-    /**
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    method: "POST",
+    body: JSON.stringify(myData),
+    headers: { "content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      postsArray.unshift(data)
+      renderPosts(postsArray)
+    });
+  clearForm()
+});
+
+/**
+ * Challenge: Send this off to the server!
+ *
+ * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ * 2. Endpoint: /posts
+ * 3. method: ???
+ * 4. Request body: ??? (Remember to turn it into JSON)
+ * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
+ */
+/**
  Challenge:
  
  * Listen for the "submit" event on the form (which will happen when the button is clicked)
