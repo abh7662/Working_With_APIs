207b4abddf34be0fc08f06403b02cef3b9ee9199
ak7662 <ak7662@gmail.com>
2021-07-06 22:21:02 +0530

    setup of capstone project

--- CapStoneProject/index.js
+++ CapStoneProject/index.js
@@ -1,66 +1,26 @@
-let postsArray = []
-
-function renderPosts(postsArray) {
-  document.querySelector("#main").innerHTML = postsArray
-    .map(
-      (arr) => `
-      <h1>${arr.title}</h1>
-        <p>${arr.body}</p>
-        <hr />
-      `
-    )
-    .join("");
-}
-function clearForm() {
-  getIds("title").value = "";
-  getIds("body").value = "";
-}
-fetch("https://apis.scrimba.com/jsonplaceholder/posts/")
-  .then((response) => response.json())
-  .then((data) => {
-    postsArray = data.slice(0, 5);
-    renderPosts(postsArray);
-  })
-
-function getIds(value) {
-  return document.getElementById(value);
-}
-let myData = {};
-document.querySelector(".btn").addEventListener("click", () => {
-  const title = getIds("title").value;
-  const body = getIds("body").value;
-  myData = {
-    title,
-    body,
-  }
-  fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
-    method: "POST",
-    body: JSON.stringify(myData),
-    headers: { "content-Type": "application/json" },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      postsArray.unshift(data)
-      renderPosts(postsArray)
-    });
-  clearForm()
-});
-
 /**
- * Challenge: Send this off to the server!
- *
- * 1. BaseURL: https://apis.scrimba.com/jsonplaceholder/
- * 2. Endpoint: /posts
- * 3. method: ???
- * 4. Request body: ??? (Remember to turn it into JSON)
- * 5. Headers: ??? (Check the JSON Placeholder API docs or past casts for help)
- */
-/**
- Challenge:
- 
- * Listen for the "submit" event on the form (which will happen when the button is clicked)
-    * (Don't forget to preventDefault on the form so it doesn't refresh your page. Google "form preventDefault" if you're not sure what I'm talking about)
- * Combine the title value and body value into an object (with a "title" property and "body" property)
- * Log the object to the console
+ * Challenge: get a random image from Unsplash and set it as the background
  * 
+ * Part 1:
+ * 
+ * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
+ * (You can change the "query" at the end to whatever theme you want)
+ * 
+ * Change the body's backgroundImage to: 
+ * `url(<insert the URL of the iamge from the API here>)`
+ * 
+ * (You may need to dig around the response body a bit to find this URL)
+ * 
+ * (Note I've already added some CSS to resize the image within the window.
+ * Instructions for this were found on CSS Tricks: 
+ * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
  */
+
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature",{
+  method: "GET"
+}).then(res => res.json())
+.then(data => {
+  console.log(data)
+  // document.body.style.backgroundImage = `url(${data.urls.full})`
+})
