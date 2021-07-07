c21b76001057a92d81120b1aab9b605ac78ccbe4
ak7662 <ak7662@gmail.com>
2021-07-07 19:35:37 +0530

    added background from unsplash

--- CapStoneProject/index.js
+++ CapStoneProject/index.js
@@ -22,5 +22,5 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 }).then(res => res.json())
 .then(data => {
   console.log(data)
-  // document.body.style.backgroundImage = `url(${data.urls.full})`
+  document.body.style.backgroundImage = `url(${data.urls.full})`
 })
