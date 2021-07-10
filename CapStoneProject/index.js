9bf4ea817e63227f8dac2554958e801fd394c3c1
ak7662 <ak7662@gmail.com>
2021-07-10 22:40:03 +0530

    promise rejection

--- CapStoneProject/index.js
+++ CapStoneProject/index.js
@@ -7,14 +7,10 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
   document.getElementById("author").textContent = `By: ${data.user.name}`
 })
 .catch(err => {
-  document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1575720303061-9a8b23e2ca92?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjU4MjIyNzI&ixlib=rb-1.2.1&q=85)"
+  document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-1505765050516-f72dcac9c60e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjU5MzY3OTU&ixlib=rb-1.2.1&q=80&w=1080)"
 })
 
-/**
-         * Challenge: get a URL for a default background image and set it here
-         * 
-         * 1. Change the query in the URL above back to something real
-         * 2. Log the image url to the console (replacing console.log(data) above)
-         * 3. Use that URL as the "default" background image to be used if 
-         *    the promise is ever rejected.
-         */
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.error(err))
