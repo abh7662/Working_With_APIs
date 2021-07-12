159c5fb2daa11afdc0a299f8ae19f37dc452a63f
ak7662 <ak7662@gmail.com>
2021-07-12 18:57:00 +0530

    weather app completed

--- CapStoneProject/index.js
+++ CapStoneProject/index.js
@@ -67,7 +67,15 @@ setInterval(() => {
 navigator.geolocation.getCurrentPosition(position => {
   fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+      console.log(data)
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      document.getElementById("weather").innerHTML = `
+        <img src=${iconUrl} alt="icon image" />
+        <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+      `
+    })
 });
 
 /**
